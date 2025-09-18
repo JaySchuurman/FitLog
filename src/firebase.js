689ca5e4f897f988c802b39179e5511b6e2eaac1
Jay Schuurman <1208455@student.roc-nijmegen.nl>
2025-09-18 14:13:08 +0200

    Login and register now work with firebase

---
+++ src/firebase.js
@@ -0,0 +1,23 @@
+// src/firebase.js
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBEYNFRL5Lfq7E7vX2ykOJ7KM9EjonfSxk",
+  authDomain: "projectlj3mbo.firebaseapp.com",
+  projectId: "projectlj3mbo",
+  storageBucket: "projectlj3mbo.appspot.com",
+  messagingSenderId: "756572762342",
+  appId: "1:756572762342:web:YOUR_APP_ID",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Export Firebase services to use in your app
+export const auth = getAuth(app);
+export const db = getFirestore(app);
+export const storage = getStorage(app);
